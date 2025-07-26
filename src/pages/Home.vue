d41664151a84a1fd250204edcaf94e5d8e0718e1
wanghao <wanghao0557@gmail.com>
2025-07-26 11:34:00 +0800

    feat: 增加pinia3.0.3

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,15 @@
 <script setup name='Home' lang='ts'>
+import {useCountStore} from '@/stores/count'
+const countStore = useCountStore()
+function increment() {
+    countStore.increment()
+}
 </script>
 <template>
-    <div>home</div>
+    <div>
+        <h1>home{{ countStore.count }}</h1>
+        <button class="bg-sky-500" @click="increment">+1</button>
+    </div>
 </template>
 <style scoped lang='scss'>
 </style>
---
+++ src/stores/count.ts
@@ -0,0 +1,18 @@
+import {defineStore} from 'pinia'
+
+export const useCountStore = defineStore('count', {
+  state: () => ({
+    count: 0
+  }),
+  getters: {
+    doubleCount: (state) => state.count * 2
+  },
+  actions: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    }
+  }
+})
